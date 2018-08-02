0d5e8baaf6e0c4fa4f1f02506d454cd4bee91347
Amy MacKinnon <amy@amy-mac.com>
2018-08-02 11:51:47 -0700

    Got upgraded jekyll working with all links and pages

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -2,23 +2,23 @@ $(document).ready(function() {
 
   $('dd a').on('click', function(event) {
     event.stopPropagation();
+    event.preventDefault();
 
     $('dd').removeClass('active');
     $(event.currentTarget).parent().addClass('active');
 
-    var filteredClass = $(event.currentTarget).html();
+    const filteredClass = $(event.currentTarget).text();
 
-    if(filteredClass == "All") {
+    if (filteredClass === "All") {
       $('div .theatre').removeClass('hide');
     } else {
       $('div .theatre').addClass('hide');
-      $('div[data-id='+ filteredClass +']').removeClass('hide');    
+      $(`div[data-id="${filteredClass}"]`).removeClass('hide');
     }
 
-    if($('div .hide').length >= 4) {
+    if ($('div .hide').length >= 4) {
       $('#mainSection hr').addClass('hide');
     }
   });
-
 
 });
